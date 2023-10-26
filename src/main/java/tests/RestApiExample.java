fa50c782014d17bf59bc8a51f68173e25146c68f
unknown <andrei.mihai3110@gmail.com>
2023-10-26 11:26:04 +0300

    comit initial

---
+++ src/main/java/tests/RestApiExample.java
@@ -0,0 +1,39 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class RestApiExample {
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void postATodoTest() {
+		File fisier = new File("data.json");
+		Faker faker = new Faker();
+		
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("title", faker.hobbit().character());
+		requestBody.put("body", faker.chuckNorris().fact());
+		
+		Response response = RestAssured.given().header("accept", "application/json")
+				.header("Content-Type", "application/json")
+				//.body(new File("data.json"))
+				.body(requestBody.toJSONString())
+				.when().post("https://keytodorestapi.herokuapp.com/api/save").then().assertThat().statusCode(200).extract().response();
+		System.out.println(response.asPrettyString());
+		String info = response.jsonPath().getString("info");
+		System.out.println(info);
+		assertEquals(info, "Todo saved! Nice job!");
+		
+	}
+
+}
