44c0860cf780ba68c73177fc5d02f8af42c98c83
unknown <andrei.mihai3110@gmail.com>
2023-11-13 11:33:42 +0200

    curs45

--- src/main/java/tests/HamcrestAssertionExample.java
+++ src/main/java/tests/HamcrestAssertionExample.java
@@ -4,6 +4,7 @@ package tests;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.testng.annotations.Test;
@@ -13,6 +14,8 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static utils.NumberChecker.*;
+import static utils.NumberIsPositive.*;
 
 public class HamcrestAssertionExample {
 	
@@ -71,6 +74,32 @@ public class HamcrestAssertionExample {
 		        "https://swapi.dev/api/films/4/", 
 		        "https://swapi.dev/api/films/5/", 
 		        "https://swapi.dev/api/films/6/"));
-	}
+		String[] array = {jsnPath.getString("climate"), jsnPath.getString("terrain"),jsnPath.getString("diameter"),jsnPath.getString("gravity"),jsnPath.getString("name") };
+		System.out.println(array[0]);
+		assertThat(array, is(not(emptyArray())));
+		assertThat(array, is(not(nullValue())));
+		System.out.println(Arrays.toString(array));
+		assertThat(array, arrayContainingInAnyOrder(
+				"Tatooine",
+				"arid", 
+				"desert", 
+				"10465", 
+				"1 standard" ));
+		assertThat(array, arrayContaining("arid", "desert", "10465", "1 standard","Tatooine"));
+		assertThat(resp.asString(), containsStringIgnoringCase("ARID") );
+		assertThat(resp.asString(), stringContainsInOrder("name","rotation_period" ));
+		//and
+		assertThat(resp.asString(),both(containsString("Tatooine")).and(containsString(diameter)));
+		//or
+		assertThat(name, either(is("Tatooine")).or(is("Tatooine2")).or(is(not("Tatooine5"))));
+		String rotation = jsnPath.getString("rotation_period");
+		String climate = jsnPath.getString("climate");
+		String gravity = jsnPath.getString("gravity");
+		System.out.println(rotation);
+		System.out.println(climate);
+		System.out.println(gravity);
+		assertThat(rotation, is(numbersOnly()));
+		assertThat(Integer.parseInt(rotation), is(positiveNumber()));
+		
+	}}
 
-}
